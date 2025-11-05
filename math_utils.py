a27ba89406b3814d6f55c6a1968750153568cfbc
ofersomberg-dev <ofersomberg@gmail.com>
2025-11-05 07:04:58 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -1,11 +1,10 @@
 def find_max_number(num1, num2, num3):
-    pass  # Replace 'pass' with code
- if num1 >= num2 and num1 >= num3:
-  return num1 
- if num2 >= num1 and num2 >= num3:
-  return num2 
-else:
-  return num3
+  if num1 >= num2 and num1 >= num3:
+   return num1 
+  if num2 >= num1 and num2 >= num3:
+    return num2 
+  else:
+    return num3
   
 def find_mean(num1, num2, num3):
   return (num1 + num2 + num3)/3
